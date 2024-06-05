a49e4ae6393380dfc3ced34ae7cb2297de42f1aa
brunolopeschagas <brunolopeschagas@gmail.com>
2024-06-05 20:45:03 -0300

    Monster Tamer - RPG Tutorial with Phaser 3 - Ep. 5 - Loading Game Assets

---
+++ src/scenes/preload-scene.js
@@ -0,0 +1,50 @@
+import { BATTLE_ASSET_KEYS, BATTLE_BACKGROUND_ASSET_KEYS, HEATL_BAR_ASSET_KEYS, MONSTER_ASSET_KEYS } from "../assets/asset-keys.js";
+import Phaser from "../lib/phaser.js";
+import { SCENE_KEYS } from "./scene-keys.js";
+
+export class PreloadScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: SCENE_KEYS.PRELOAD_SCENE,
+        });
+        console.log(SCENE_KEYS.PRELOAD_SCENE);
+    }
+
+    preload() {
+        this.load.image(
+            BATTLE_BACKGROUND_ASSET_KEYS.FOREST,
+            'assets/images/monster-tamer/battle-backgrounds/forest-background.png'
+        );
+        this.load.image(
+            BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND,
+            'assets/images/kenneys-assets/ui-space-expansion/custom-ui.png'
+        );
+        this.load.image(
+            HEATL_BAR_ASSET_KEYS.RIGHT_CAP,
+            'assets/images/kenneys-assets/ui-space-expansion/barHorizontal_green_right.png'
+        );
+        this.load.image(
+            HEATL_BAR_ASSET_KEYS.LEFT_CAP,
+            'assets/images/kenneys-assets/ui-space-expansion/barHorizontal_green_left.png'
+        );
+        this.load.image(
+            HEATL_BAR_ASSET_KEYS.MIDDLE,
+            'assets/images/kenneys-assets/ui-space-expansion/barHorizontal_green_mid.png'
+        );
+
+        this.load.image(
+            MONSTER_ASSET_KEYS.CARNODUSK,
+            'assets/images/monster-tamer/monsters/carnodusk.png'
+        );
+
+        this.load.image(
+            MONSTER_ASSET_KEYS.IGUANIGNITE,
+            'assets/images/monster-tamer/monsters/iguanignite.png'
+        );
+    }
+
+    create() {
+        this.textures.get('background');
+        this.add.image(0,0, BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
+    }
+}
