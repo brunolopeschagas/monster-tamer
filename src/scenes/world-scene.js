4fe5d535622c213a037f151e6f23b389ce4724fe
brunolopeschagas <brunolopeschagas@gmail.com>
2025-01-13 20:14:06 -0300

    Monster Tamer - RPG Tutorial with Phaser 3 - Ep. 38 - World Scene Player Input Part 1

--- src/scenes/world-scene.js
+++ src/scenes/world-scene.js
@@ -2,18 +2,23 @@ import Phaser from '../lib/phaser.js';
 import { WORD_ASSET_KEYS } from '../assets/asset-keys.js';
 import { SCENE_KEYS } from "./scene-keys.js";
 import { Player } from '../world/characters/player.js';
-
-const TILE_SIZE = 64;
+import { Controls } from '../utils/controls.js';
+import { DIRECTION } from '../common/direction.js';
+import { TILE_SIZE } from '../config.js';
 
 /**@type {import('../types/typedef.js').Coordinate} */
 const PLAYER_POSITION = Object.freeze({
-    x: 1 * TILE_SIZE,
-    y: 1 *  TILE_SIZE,
+  x: 1 * TILE_SIZE,
+  y: 1 * TILE_SIZE,
 });
 
-export class WorldScene extends Phaser.Scene{
+export class WorldScene extends Phaser.Scene {
 
-    #player;
+  /**@type {Player} */
+  #player;
+
+  /**@type {Controls} */
+  #controls;
 
   constructor() {
     super({
@@ -21,11 +26,20 @@ export class WorldScene extends Phaser.Scene{
     });
   }
 
-  create(){
-    this.add.image(0,0, WORD_ASSET_KEYS.WORLD_BACKGROUND, 0).setOrigin(0);
+  create() {
+    this.add.image(0, 0, WORD_ASSET_KEYS.WORLD_BACKGROUND, 0).setOrigin(0);
     this.#player = new Player({
-        scene: this,
-        position: PLAYER_POSITION,
+      scene: this,
+      position: PLAYER_POSITION,
     });
+
+    this.#controls = new Controls(this);
+  }
+
+  update(){
+    const selectedDirection = this.#controls.getDirectionKeyJustPressed();
+    if(selectedDirection !== DIRECTION.NONE){
+      this.#player.moveCharacter(selectedDirection);
+    }
   }
 }
--- src/world/characters/player.js
+++ src/world/characters/player.js
@@ -4,6 +4,7 @@ import { Character } from "../characters/character.js";
 
 /**@typedef {Omit<import("../characters/character").CharacterConfig, 'assetKey' | 'assetFrame'>} PlayerConfig */
 export class Player extends Character{
+ 
     /**
      * 
      * @param {PlayerConfig} config 
